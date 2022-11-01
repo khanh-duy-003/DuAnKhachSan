1267b7f87e878cac55bf16e1e3604896faa5bca9
duypk <duypk@unit.com.vn>
2022-11-01 21:18:21 +0700

    [duypk] commit

---
+++ dev/DuAnKhachSan_dev/src/main/java/com/vn/dev/DuAnKhachSanDevApplication.java
@@ -0,0 +1,13 @@
+package com.vn.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnKhachSanDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnKhachSanDevApplication.class, args);
+	}
+
+}
---
+++ dev/DuAnKhachSan_dev/src/test/java/com/vn/dev/DuAnKhachSanDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnKhachSanDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
